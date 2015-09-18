9b72f0fef161645f100bd76480479ef6fcc39362
John McAvey <john@mcavey.net>
2015-09-18 16:06:10 -0500

    Added Build 1.0

--- src/Tests/ClassB.java
+++ src/Tests/ClassB.java
@@ -11,15 +11,10 @@ public class ClassB extends SQLRow {
     public ClassB(){
         this.repeats = 0;
         this.name = "Neat, this is a child.";
-        //this.nonLoop = null;
     }
 
     public ClassB(ClassA parent){
         repeats = 20;
         name = "I'm glad you're here!";
-        //nonLoop = new ClassA();
-        //this.nonLoop = parent;
-        //nonLoop.name = "Hello, second item";
-        //nonLoop.numRepeats = 40;
     }
 }
